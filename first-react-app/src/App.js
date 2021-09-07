cbc6b85c06cce919c589209dbe337f3873d8c0f3
vinceagocs <vince.agocs@gmail.com>
2021-09-07 09:46:09 +0200

    add app components

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
           First react app
         </h1>
-      
       </header>
     </div>
   );
