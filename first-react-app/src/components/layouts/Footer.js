f8a0f619cf23ee579bffbaf873d5f2d393a550f5
vinceagocs <vince.agocs@gmail.com>
2021-09-07 12:20:34 +0200

    add footer datas

--- first-react-app/src/components/layouts/Footer.js
+++ first-react-app/src/components/layouts/Footer.js
@@ -6,9 +6,9 @@ const Footer = props => {
         <footer className="footer">
             <h1>VPT-week3</h1>
             <div className="footer-tabs">
-                <h3 className="tab">Categories</h3>
-                <h3 className="tab">Favorites</h3>
-                <h3 className="tab">Random</h3>
+                <h3 className="tab">Tomi</h3>
+                <h3 className="tab">Plantelo</h3>
+                <h3 className="tab">Vince</h3>
             </div>
         </footer>
     )
