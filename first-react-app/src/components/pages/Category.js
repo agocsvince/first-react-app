1a5d83ed9f8f106b62039fb8dc7ba13e89b323f3
KT <krakovszkyt@gmail.com>
2021-09-08 10:32:03 +0200

    Add links to product pages

--- first-react-app/src/components/pages/Category.js
+++ first-react-app/src/components/pages/Category.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import { useState, useEffect } from 'react'
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 
 const Category  = () => {
     const [category, setCategory] = useState([]);
@@ -19,7 +19,7 @@ const Category  = () => {
 
     return (
         <div>
-            {category.map((product) => (<h4 key={product.id}>{product.product_name}</h4>))}
+            {category.map((product) => (<h4 key={product.id}><a href={`https://fr-en.openfoodfacts.org/products/${product.id}.json`}>{product.product_name}</a></h4>))}
         </div>
     )
 }
