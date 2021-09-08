1f61adfcd5f121d50b2b996bb324827a9ee9e264
vinceagocs <vince.agocs@gmail.com>
2021-09-08 13:19:05 +0200

    remove unnecessary folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import './css/style.css'
-import react from 'react';
+import react, { useEffect } from 'react';
 import  { BrowserRouter as Router, Route } from 'react-router-dom';
 import CategoryList from './components/pages/CategoryList';
 import Category from './components/pages/Category';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
 import ListView from './components/pages/ListView'
+import PageContext from './components/pages/ListView';
 
 const App = props => {
+  let currentPage = 1
+
+  const nextPage = e => {
+    currentPage++
+  }
+
+  const prevPage = e => {
+    currentPage--
+  }
+
   return (
       <div className="container">
         <Router>
@@ -16,8 +27,10 @@ const App = props => {
           <div id="main">
             <Route path="/">
               <div className="grid">
-                <ListView />
+                <ListView page={currentPage}/>
               </div>
+              <button onClick={prevPage}>Prev</button>
+              <button onClick={nextPage}>Next</button>
             </Route>
           </div>
           <Route path="/details/:food"/>
--- src/components/pages/ListView.js
+++ src/components/pages/ListView.js
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '../Product';
 
+
 const ListView = props => {
     const [products, setProducts] = useState({
         products: []
     });
 
+    console.log(props.page)
+
     useEffect(() => {
-        axios.get('https://world.openfoodfacts.org/products.json')
-            .then(res => setProducts({ products: res.data.products }))
-    }, [])
+        axios.get(`https://world.openfoodfacts.org/search?page=${props.page}&page_size=100.json`)
+            .then(res => {
+                console.log(res)
+                setProducts({ products: res.data.products });
+            })
+    }, [props.page])
 
     return products.products.map((product) => 
     <Product key={product.id} product={product} />)
