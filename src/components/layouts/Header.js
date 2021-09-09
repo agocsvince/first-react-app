f231af4600d20b25713dd4381391037550cae9f3
vinceagocs <vince.agocs@gmail.com>
2021-09-09 12:16:33 +0200

    fix header title path

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = props => {
     return (
         <header className="header">
-            <Link className="title" to="/"><h1>YourFood</h1></Link>
+            <Link className="title" to="/page/1"><h1>YourFood</h1></Link>
             <div className="header-tabs">
                 <Link className="tab" to="/categories">Categories</Link>
                 <Link className="tab" to="/favorites">Favorites</Link>
